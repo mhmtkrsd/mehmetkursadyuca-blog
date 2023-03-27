2c66d85014f720a35a060ff86924bdc7b4dcdce1
Mehmet Kürşad Yuca <mehmetkursadyuca@gmail.com>
2023-03-27 16:29:35 +0300

    Config settings

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '930a0dbc88774a83a03eefc064ad2454',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Mehmet Kürşad YUCA',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Mehmet Kursad YUCA',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'MehmetKursadYuca',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'mhmtkrsd',
+  github: 'mhmtkrsd',
+  linkedin: 'https://www.linkedin.com/in/mehmet-k%C3%BCr%C5%9Fad-yuca-b391a213a/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
