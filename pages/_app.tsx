0afb28680b3abda1db791dbf12ac9e227e3b30ea
Mehmet Kürşad Yuca <mehmetkursadyuca@gmail.com>
2023-04-04 00:56:55 +0300

    Vercel analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,7 @@ import {
   posthogConfig,
   posthogId
 } from '@/lib/config'
+import { Analytics } from '@vercel/analytics/react';
 
 if (!isServer) {
   bootstrap()
@@ -61,5 +62,8 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return <> 
+  <Analytics />
+  <Component {...pageProps} />
+  </>
 }
